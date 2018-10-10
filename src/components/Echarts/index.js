b1592da581c887bc4eee58159efb217be45fdc87
hnlinzhi19 <hnyanlin19@163.com>
2018-10-10 16:44:07 +0800

    测试代码

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import { WebView, View, StyleSheet } from 'react-native';
+import { WebView, View, StyleSheet, Platform } from 'react-native';
 import renderChart from './renderChart';
 import echarts from './echarts.min';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log('echart init')
+    this.setNewOption = this.setNewOption.bind(this);
+  }
+  
+
   componentWillReceiveProps(nextProps) {
     if(nextProps.option !== this.props.option) {
       this.refs.chart.reload();
     }
+  }
+
+  setNewOption(option) {
+    this.refs.chart.postMessage(JSON.stringify(option));
   }
 
   render() {
@@ -21,7 +33,7 @@ export default class App extends Component {
             height: this.props.height || 400,
             backgroundColor: this.props.backgroundColor || 'transparent'
           }}
-          scalesPageToFit={false}          
+          scalesPageToFit={Platform.OS !== 'ios'}
           source={require('./tpl.html')}
           onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
         />
