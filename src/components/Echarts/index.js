95f078cab8c561f2e235a8dbe77fa7ddad364495
hnlinzhi19 <hnyanlin19@163.com>
2018-10-10 15:06:28 +0800

    增加log 切换到echart4

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
-import { WebView, View, StyleSheet, Platform } from 'react-native';
+import { WebView, View, StyleSheet } from 'react-native';
 import renderChart from './renderChart';
 import echarts from './echarts.min';
 
 export default class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.setNewOption = this.setNewOption.bind(this);
-  }
-  
-
   componentWillReceiveProps(nextProps) {
     if(nextProps.option !== this.props.option) {
       this.refs.chart.reload();
     }
-  }
-
-  setNewOption(option) {
-    this.refs.chart.postMessage(JSON.stringify(option));
   }
 
   render() {
@@ -32,7 +21,7 @@ export default class App extends Component {
             height: this.props.height || 400,
             backgroundColor: this.props.backgroundColor || 'transparent'
           }}
-          scalesPageToFit={Platform.OS !== 'ios'}
+          scalesPageToFit={false}          
           source={require('./tpl.html')}
           onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
         />
--- src/index.js
+++ src/index.js
@@ -3,15 +3,10 @@ import { WebView, View } from 'react-native';
 import { Container, Echarts } from './components'
 
 export default class App extends Component {
-
-  setNewOption(option) {
-    this.chart.setNewOption(option);
-  }
-
   render() {
     return (
       <Container width={this.props.width}>
-        <Echarts {...this.props} ref={e => this.chart = e}/>
+        <Echarts {...this.props} />
       </Container>
     );
   }
